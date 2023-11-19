646a04a6a18298274c7a2b85478f2802eb9e37c6
joshuaotters <joshuasemwanga@gmail.com>
2023-11-19 16:50:06 +0300

    connect to mongo atlas

--- myApp.js
+++ myApp.js
@@ -1,4 +1,37 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+
+//connect to remote db::mongodb atlas
+//mongodb+srv://joshuasemwanga:7QhPkVPgiuxhxVwW@cluster0.scstgus.mongodb.net/?retryWrites=true&w=majority
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://joshuasemwanga:7QhPkVPgiuxhxVwW@cluster0.scstgus.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);//end of MongoDB connection parameters
+
 
 
 let Person;
@@ -50,6 +83,8 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
