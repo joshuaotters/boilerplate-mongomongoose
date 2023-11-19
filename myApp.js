3c1f1b4b1f92e88fa42565834c9d94f8e230b3ef
joshuaotters <joshuasemwanga@gmail.com>
2023-11-19 17:47:23 +0300

    updated connection parameters

--- myApp.js
+++ myApp.js
@@ -1,36 +1,9 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 
-//connect to remote db::mongodb atlas
-//mongodb+srv://joshuasemwanga:7QhPkVPgiuxhxVwW@cluster0.scstgus.mongodb.net/?retryWrites=true&w=majority
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://joshuasemwanga:7QhPkVPgiuxhxVwW@cluster0.scstgus.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);//end of MongoDB connection parameters
+//connect to remote db instance::mongodb atlas
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
