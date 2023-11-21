1e8085fdfd073d6c35d07a47de0a01b4f1a8ef8b
joshuaotters <joshuasemwanga@gmail.com>
2023-11-21 10:19:24 +0300

    changed favouriteFoods field type

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ const personSchema = new mongoose.Schema({
     require: true
   },
   age: Number,
-  favoriteFoods: String
+  favoriteFoods: [String]
 
 });
 const Person = mongoose.model("Person", personSchema);
