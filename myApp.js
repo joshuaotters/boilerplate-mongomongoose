81823a5ac8a130f89d43cd7a1f1ff0d073cf1594
joshuaotters <joshuasemwanga@gmail.com>
2023-11-22 01:25:58 +0300

    refactored findById  update and save

--- myApp.js
+++ myApp.js
@@ -64,16 +64,21 @@ const findPersonById = (personId, done) => {
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-  Person.findById(personId, (err, personFound) => {
-    if(err) return console.error(err);
-    personFound.favoriteFoods.push(foodToAdd);
-    personFound.save(err, (updatedPerson) => {
-      if(err) return console.error(err)
-        done(null, updatedPerson);
-    });
-    
-  });
+  const foodToAdd = 'hamburger';
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
